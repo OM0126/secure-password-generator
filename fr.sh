7e2794c546e958957c49c86a8a01340bd6ab9d19
om yengantiwar <omgithub01@gmail.com>
2026-06-10 15:40:32 +0530

    Delete fr.sh

--- fr.sh
+++
@@ -1,9 +0,0 @@
-  GNU nano 7.2                                                                                 3.sh                                                                                           
-#!/bin/bash 
-name="om"
-age=19
-
-echo "Name: $name"
-echo "Age:  $age"
-
-
